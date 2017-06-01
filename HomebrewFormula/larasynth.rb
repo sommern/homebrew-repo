9a052b579e6261fedff5229899b0adb3f08ea944
Nathan Sommer <nsommer@wooster.edu>
2017-06-01 15:02:26 -0400

    Updated sha256

--- HomebrewFormula/larasynth.rb
+++ HomebrewFormula/larasynth.rb
@@ -2,6 +2,7 @@ class Larasynth < Formula
   desc "Larasynth can learn to turn the knobs of your synthesizer for you."
   homepage "https://github.com/sommern/larasynth"
   url "https://github.com/sommern/larasynth/releases/download/v0.2.2/larasynth-source-0.2.2.tar.gz"
+  sha256 "b791d139f630f69f14a858e50e31db1b4aef960f51d223cbeb27a107bc0e9b54"
 
   def install
     system "./configure", "--disable-debug",
