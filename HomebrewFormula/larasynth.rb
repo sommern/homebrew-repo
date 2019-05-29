9335ac2852fd0004446acff5ec4b10e212a6df2b
Nathan Sommer <nsommer@wooster.edu>
2019-05-29 16:03:10 -0400

    Larasynth v0.2.4

--- HomebrewFormula/larasynth.rb
+++ HomebrewFormula/larasynth.rb
@@ -1,8 +1,8 @@
 class Larasynth < Formula
   desc "Larasynth can learn to turn the knobs of your synthesizer for you."
   homepage "https://github.com/sommern/larasynth"
-  url "https://github.com/sommern/larasynth/releases/download/v0.2.3/larasynth-source-0.2.3.tar.gz"
-  sha256 "82aecbefe3db0163a7a7aa913b77ee1b5046acbcc92e6671e00aea46ac872606"
+  url "https://github.com/sommern/larasynth/releases/download/v0.2.4/larasynth-source-0.2.4.tar.gz"
+  sha256 "81658a9509ac540b1e802e409afd143b31c6f3e312cdedb42b75f9c05c010625"
 
   def install
     system "./configure", "--disable-debug",
